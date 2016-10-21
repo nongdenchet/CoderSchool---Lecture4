7a2c9e13695079b120c1490926275011d451c055
Quan Vu <nongdenchet8435848@gmail.com>
2016-10-21 16:11:44 +0700

    done

--- app/src/main/java/apidez/com/week4/CollapsingToolbarActivity.java
+++ app/src/main/java/apidez/com/week4/CollapsingToolbarActivity.java
@@ -1,9 +1,11 @@
 package apidez.com.week4;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.AppBarLayout;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 
 import apidez.com.week4.adapter.UserAdapter;
 import butterknife.BindView;
@@ -14,6 +16,9 @@ public class CollapsingToolbarActivity extends AppCompatActivity {
     @BindView(R.id.rvUsers)
     RecyclerView rvUsers;
 
+    @BindView(R.id.appBar)
+    AppBarLayout appBarLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,5 +26,32 @@ public class CollapsingToolbarActivity extends AppCompatActivity {
         ButterKnife.bind(this);
         rvUsers.setAdapter(new UserAdapter());
         rvUsers.setLayoutManager(new LinearLayoutManager(this));
+
+        appBarLayout.addOnOffsetChangedListener(new AppBarStateChangeListener() {
+            @Override
+            public void onStateChanged(AppBarLayout appBarLayout, State state) {
+                switch (state) {
+                    case COLLAPSED:
+                        break;
+                    case IDLE:
+                        break;
+                }
+            }
+        });
+    }
+
+    private void hide() {
+        View decorView = getWindow().getDecorView();
+        // Hide Status Bar.
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+    }
+
+    private void show() {
+        // Show status bar
+        View decorView = getWindow().getDecorView();
+        // Show Status Bar.
+        int uiOptions = View.SYSTEM_UI_FLAG_VISIBLE;
+        decorView.setSystemUiVisibility(uiOptions);
     }
 }
