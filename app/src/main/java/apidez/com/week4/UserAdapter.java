3f52cfd666ce9ded50afc987acce504bc512394e
Quan Vu <nongdenchet8435848@gmail.com>
2016-10-30 21:26:03 +0700

    remove unused class

--- app/src/main/java/apidez/com/week4/UserAdapter.java
+++ app/src/main/java/apidez/com/week4/UserAdapter.java
@@ -7,7 +7,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import apidez.com.week4.R;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
