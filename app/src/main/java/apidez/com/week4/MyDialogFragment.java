6d16eb0591c85009a6f673d5cdae2201de31af57
Quan Vu <nongdenchet8435848@gmail.com>
2016-10-31 14:45:05 +0700

    refactor

--- app/src/main/java/apidez/com/week4/MyDialogFragment.java
+++ app/src/main/java/apidez/com/week4/MyDialogFragment.java
@@ -1,11 +1,11 @@
 package apidez.com.week4;
 
-import android.app.Dialog;
-import android.support.annotation.NonNull;
-import android.support.design.widget.BottomSheetBehavior;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.design.widget.BottomSheetDialogFragment;
-import android.support.design.widget.CoordinatorLayout;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by nongdenchet on 10/28/16.
@@ -13,32 +13,10 @@ import android.view.View;
 
 public class MyDialogFragment extends BottomSheetDialogFragment {
 
-    private BottomSheetBehavior.BottomSheetCallback mBottomSheetBehaviorCallback =
-            new BottomSheetBehavior.BottomSheetCallback() {
-
-                @Override
-                public void onStateChanged(@NonNull View bottomSheet, int newState) {
-                    if (newState == BottomSheetBehavior.STATE_HIDDEN) {
-                        dismiss();
-                    }
-
-                }
-
-                @Override
-                public void onSlide(@NonNull View bottomSheet, float slideOffset) {
-                }
-            };
-
+    @Nullable
     @Override
-    public void setupDialog(Dialog dialog, int style) {
-        super.setupDialog(dialog, style);
-        View contentView = View.inflate(getContext(), R.layout.fragment_bottom_sheet, null);
-        dialog.setContentView(contentView);
-        CoordinatorLayout.LayoutParams params = (CoordinatorLayout.LayoutParams) ((View) contentView.getParent()).getLayoutParams();
-        CoordinatorLayout.Behavior behavior = params.getBehavior();
-        if (behavior != null && behavior instanceof BottomSheetBehavior) {
-            ((BottomSheetBehavior) behavior).setBottomSheetCallback(mBottomSheetBehaviorCallback);
-            ((BottomSheetBehavior) behavior).setPeekHeight(500);
-        }
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_bottom_sheet, container, false);
     }
 }
